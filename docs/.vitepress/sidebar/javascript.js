a9d640aefc473e1741cf4c68b09760b0e20a2323
Gleyson Sampaio <glysns@hotmail.com>
2023-01-29 13:54:30 -0300

    ajustes

--- docs/.vitepress/sidebar/javascript.js
+++ docs/.vitepress/sidebar/javascript.js
@@ -2,9 +2,7 @@ export const javascriptsb = [
   {
     text: 'Fundamentos',collapsible: true,
     items: [
-      { text: 'O que é Java Script ?', link:'/topicos/linguagens/javascript/index' },
-      { text: 'Essencial', link:'/soon'   },
-      { text: 'Ambiente de desenvolvimento', link:'/soon' },
+      { text: 'O que é Java Script ?', link:'/topicos/linguagens/javascript/index' }
     ]
   },
 ]
