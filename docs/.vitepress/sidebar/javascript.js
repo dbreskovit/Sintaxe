e3d9ae02585b99b91d6dfb761dd0fb865eeac2de
Carlos Henrique <carlos.nascto1604@gmail.com>
2023-02-06 20:13:55 -0300

    alterações

--- docs/.vitepress/sidebar/javascript.js
+++ docs/.vitepress/sidebar/javascript.js
@@ -11,11 +11,8 @@ export const javascriptsb = [
     text: 'Básico',collapsible: true, collapsed: true,
     items: [
       { text: 'Sintaxe', link:'/topicos/linguagens/javascript/sintaxe' },
-      { text: 'Palavras reservadas', link:'/topicos/linguagens/javascript/palavras-reservadas' },
-      { text: 'Anatomia e Escopo', link:'/topicos/linguagens/javascript/anatomia-escopo' },
       { text: 'Tipos e Variáveis', link:'/topicos/linguagens/javascript/tipos-variaveis' },
-      { text: 'Declaração e Atribuição', link:'/topicos/linguagens/javascript/declaracao-atribuicao' },
-      { text: 'Operadores', link:'/topicos/linguagens/javascript/operadores'}
+      { text: 'Palavras Reservadas', link:'/topicos/linguagens/javascript/palavras-reservadas' }
     ]
   },
 ]
